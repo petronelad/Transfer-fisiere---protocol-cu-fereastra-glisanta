1c7626c5c79c756fcc596f8b2fd5921971b8bea1
petroneladumbrava <petronela.dumbrava@student.tuiasi.ro>
2022-01-15 17:52:24 +0200

    interface

--- SWsender.py
+++ SWsender.py
@@ -1,8 +1,10 @@
 import socket
+import threading
 import packetS
+import time
 
 
-TIMER = 0.5
+TIMER = 2
 PACKET_SIZE = 200
 WINDOW_SIZE = 8
 
@@ -11,8 +13,9 @@ WINDOW_SIZE = 8
         create_buffer()
     all_packets = buffer with packets
     id_packet_in_buffer = number of packets in buffer
-    Opening file with the text to send, creating packets and adding them in a buffer
-'''
+    Opening file with the text to send, creating packets and adding them in a buffer'''
+
+
 def create_buffer(file_to_send):
     try:
         file = open(file_to_send, 'rb')
@@ -20,18 +23,17 @@ def create_buffer(file_to_send):
         print("The file can't be opened")
         return
     all_packets = []
-    id_packet_in_buffer = 0
+    no_packet_in_buffer = 0
     while True:
         packet = packetS.Packet()
-        packet.data = file.read(PACKET_SIZE)
+        packet.data =file.read(PACKET_SIZE)
         if not packet.data:
             break
-        id_packet_in_buffer += 1
-        flag = packetS.State.ON_HOLD
-        packet.create_packet(id_packet_in_buffer, flag)
+        no_packet_in_buffer += 1
+        packet.create_packet(no_packet_in_buffer, packetS.State.ON_HOLD)
         all_packets.append(packet)
     file.close()
-    return all_packets
+    return all_packets, no_packet_in_buffer
 
 
 '''
@@ -40,18 +42,39 @@ def create_buffer(file_to_send):
     AF_INET represents  the address families, used for the first argument
     to the socket().
     SOCK_DGRAM is a cst that represents  the socket type, used for the 2nd
-    argument to socket().
-'''
-def send(file_to_send):
-    buffer = create_buffer(file_to_send)
-    socket_for_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    argument to socket().'''
 
-    # socket.bind(address)
+
+def send(file_to_send):
+
     # Bind the socket to address. The socket must not already be bound.
     # (The format of address depends on the address family — see above.)
     # Raises an auditing event socket.bind with arguments self, address.
+
+    socket_for_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_for_send.bind(packetS.SOURCE_ADDRESS)
+    socket_for_send.settimeout(TIMER)
+
+    print("to send")
+
+    buffer, number_packets = create_buffer(file_to_send)
+    
+
+    start_time = time.time()
+    
     number_elem_in_win = 0
     packet_index = 0
+
+    # threading.start_new_thread()
+
+    while True:
+        try:
+            new_packet, client_address = socket_for_send.recvfrom(packetS.DESTINATION_ADDRESS[1])
+
+
+        except:
+            break
+
     # while number_elem_in_win < WINDOW_SIZE:
     #     for packet in buffer:
     #         if packet.flag == 1:
@@ -62,8 +85,12 @@ def send(file_to_send):
     #             print(packet.data)
 
     for packet in buffer:
-        if packet.flag == packetS.State.ON_HOLD:
-            packet.send_packet(socket_for_send)
+        if packet.flag == packetS.State.ON_HOLD and number_elem_in_win < WINDOW_SIZE:
+            packet.flag = packetS.State.SEND
+            id_packet_in_bytes = packet.id_packet.to_bytes(4, byteorder='little', signed=True)
+            socket.sendto(packet.data + id_packet_in_bytes, packetS.DESTINATION_ADDRESS)
+            print(packet.data)
+    
     socket_for_send.close()
 
 
--- interface.py
+++ interface.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from tkinter.filedialog import askopenfile
+import SWsender
 
 class GUI():
     def __init__(self):
@@ -9,7 +10,7 @@ class GUI():
         window.title('Transfer de fisiere')
 
         #dimensiuni fereastra
-        canvas = tk.Canvas(window, height=500, width=1000)
+        canvas = tk.Canvas(window, height=700, width=1200)
         canvas.pack()
 
         #imaginea de fundal
@@ -19,60 +20,75 @@ class GUI():
 
         #casuta pentru Sender
         SenderView = tk.Frame(window, bg='#000000', bd=5)
-        SenderView.place(relx=0.25, rely=0.05, relwidth=0.45, relheight=0.5, anchor='n')
+        SenderView.place(relx=0.25, rely=0.05, relwidth=0.46, relheight=0.6, anchor='n')
         label1 = tk.Label(SenderView, text='Sender')
         label1.place(relwidth=1, relheight=1)
 
         #casuta pentru Receiver
         ReceiverView = tk.Frame(window, bg='#000000', bd=5)
-        ReceiverView.place(relx=0.75, rely=0.05, relwidth=0.45, relheight=0.5, anchor='n')
+        ReceiverView.place(relx=0.75, rely=0.05, relwidth=0.47, relheight=0.6, anchor='n')
         label = tk.Label(ReceiverView, text='Receiver')
         label.place(relwidth=1, relheight=1)
 
-        #buton Connect
-        buttonC = tk.Button(text="Send", bg='#00FF00')
-        buttonC.place(relx=0.35, rely=0.6, relwidth=0.1, relheight=0.1)
-
-        #buton Disconnect
-        buttonD = tk.Button(text="Disconnect", bg='#FF0000')
-        buttonD.place(relx=0.55, rely=0.6, relwidth=0.1, relheight=0.1)
+        #terminal box
+        terminal_box = tk.Text(window, font='Helvetica 10')
+        terminal_box.place(relx=0.1, rely=0.8, relwidth=0.8, relheigh=0.15)
 
         #buton de adaugare fisier
         browse = tk.StringVar()
         browse_btn = tk.Button(window, textvariable=browse, command=lambda: open_file(), font='Arial', bg='#ff9933',
                                fg='white', height=2, width=15)
-        browse.set('Adauga fisier')
-        browse_btn.place(relx=0.44, rely=0.8, relwidth=0.15, relheight=0.15)
+        browse.set('Add file')
+        browse_btn.place(relx=0.08, rely=0.7, relwidth=0.1, relheight=0.07)
+
+        # TO DO: 
+        # button for delete content from text box (between ADD file and SEND)
+
+        # text_box = tk.Text(SenderView, height=500, width=500, padx=15, pady=15, font='Helvetica 10')
+        # text_box.place(relx=0.5, rely=0.05, relwidth=0.9, relheight=0.9, anchor='n')
+        entry_box = tk.Entry(SenderView, font='Helvetica 10')
+        entry_box.place(relwidth=0.65, relheight=1)
 
         #adaugare fisier
         def open_file():
-            print("Fisierul s-a deschis!")
             browse.set("Se incarca...")
             file = askopenfile(parent=window, mode='rb', title='Alege un fisier', filetype=[('Fisier text', '*.txt')])
             browse.set("Incarcat")
 
-            #flag=0
-
+            readFile = ''
             if file:
-                #print("Fisierul s-a incarcat.")
                 readFile = file.read()
-                #print(citireFisier)
 
             #afisarea textului in fereastra
-            readFile = ''
-            text_box = tk.Text(SenderView, height=500, width=500, padx=15, pady=15)
-            text_box.insert(1.0, readFile)
-            text_box.tag_configure('center', justify='center')
-            text_box.tag_add('center',1.0,'end')
-            text_box.place(relx=0.5, rely=0.05, relwidth=0.9, relheight=0.9, anchor='n')
+            entry_box.insert(END, readFile)
+            terminal_box.insert(END,"Fisierul s-a incarcat!\n")
+                                # text_box.tag_add('center',1.0,'end')
 
-            #casuta de introdus text
-            '''
-            frame = tk.Frame(fereastra, bg='#ffffff', bd=5)
-            frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
-            entry = tk.Entry(frame, font=40)
-            entry.place(relwidth=0.65, relheight=1)
-            '''
+        #TO DO: verify how to do the first line not blank
+        def send_pack_callback():
+            if not entry_box.get():
+                 terminal_box.insert(END, "Nothing to send!\n")
+            else:
+                print("-------------------\n")
+                print(entry_box.get())
+                print("-------------------\n")                
+                SWsender.send(entry_box.get())
+
+        # buton Send
+        button_send = tk.Button(text="Send", bg='#00FF00', command=lambda: send_pack_callback())
+        button_send.place(relx=0.20, rely=0.7, relwidth=0.1, relheight=0.07)
+
+        # buton Disconnect
+        buttonD = tk.Button(text="Disconnect", bg='#FF0000')
+        buttonD.place(relx=0.70, rely=0.7, relwidth=0.1, relheight=0.07)
+
+        #casuta de introdus text -> this is done with text_box from sender
+        '''
+        frame = tk.Frame(fereastra, bg='#ffffff', bd=5)
+        frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+        entry = tk.Entry(frame, font=40)
+        entry.place(relwidth=0.65, relheight=1)
+        '''
         window.mainloop()
 
 if __name__ == "__main__":
