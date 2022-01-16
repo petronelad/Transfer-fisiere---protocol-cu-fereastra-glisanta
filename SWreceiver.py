aab14782cc6a2ee938706506fb67eb208cf9f70c
petroneladumbrava <petronela.dumbrava@student.tuiasi.ro>
2022-01-16 23:16:51 +0200

    Revert "SW"
    This reverts commit 3c34bfabdef1f36a405562f5d12353a047e4ccb1.

--- SWreceiver.py
+++ SWreceiver.py
@@ -1,14 +1,14 @@
 import socket
-from struct import pack
 import packetS
 
 '''
     receive() - receives packets, 
-    recvfrom return a pair(bytes, address) -> data received and address of the socket sender
+    recvfrom return a pair(bytes, address) -> data received and adsress of the socket sender
 '''
 
+#primesc pachetul si trimit raspuns
 
-def receive_packet_and_send_ack(file_received, socket_received):
+def receive(file_received):
     try:
         file = open(file_received, 'wb')
     except IOError:
@@ -17,44 +17,21 @@ def receive_packet_and_send_ack(file_received, socket_received):
     id_packet_in_buffer = 0
     all_packets = []
     packet = packetS.Packet()
-
-    while True:
-
-        # packet.receive_packet(socket_received)
-        data_and_id_in_bytes, source_address = socket_received.recvfrom(1024)
-        packet.id_packet = int.from_bytes(data_and_id_in_bytes[0:4], byteorder='little', signed=True)
-        packet.flag = packetS.State(int.from_bytes(data_and_id_in_bytes[4:8], byteorder='little', signed=True))
-        packet.data = data_and_id_in_bytes[4:]
-        if not packet.data:
-            break
-
-        if id_packet_in_buffer == packet.id_packet:
-            print("expected packet ", id_packet_in_buffer, "\nreceived packet ", packet.id_packet)
-            print("ack can be sent")
-            socket_received.sendto(packet.send_response(), source_address)
-            packet.flag = packetS.State.RECEIVED
-            id_packet_in_buffer += 1
-
-            all_packets.append(packet)
-            print(packet.data)
-            file.write(packet.data)
-        else:
-            print("lost packet")
-            print(
-                id_packet_in_buffer.to_bytes(4, byteorder='little', signed=True) + packetS.State.UNKNOWN.value.to_bytes(
-                    4, byteorder='little', signed=True))
-            socket_received.sendto(
-                id_packet_in_buffer.to_bytes(4, byteorder='little', signed=True) + packetS.State.UNKNOWN.value.to_bytes(
-                    4, byteorder='little', signed=True), source_address)
-    file.close()
-
-
-def receive(file_received):
     socket_received = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     socket_received.bind(packetS.DESTINATION_ADDRESS)
-    receive_packet_and_send_ack(file_received, socket_received)
+    while True:
+        packet.receive_packet(socket_received)
+        if not packet.data:
+            break
+        id_packet_in_buffer += 1
+        packet.flag = packetS.State.RECEIVED
+        all_packets.append(packet)
+        file.write(packet.data)
+    file.close()
     socket_received.close()
 
 
 if __name__ == '__main__':
+
     receive("file_received")
+
