ad53411cdec0e667375c09133a1f209300da9099
petroneladumbrava <petronela.dumbrava@student.tuiasi.ro>
2022-01-16 13:22:43 +0200

    connection sender

--- SWsender.py
+++ SWsender.py
@@ -9,7 +9,11 @@ STOP_TIME = 5
 PACKET_SIZE = 200
 WINDOW_SIZE = 8
 
+start_time = STOP_TIME
+my_thread = _thread.allocate_lock()
+id_first_elem_in_window = 0
 
+    
 '''
         create_buffer()
     all_packets = buffer with packets
@@ -46,39 +50,27 @@ def create_buffer(file_to_send):
     argument to socket().'''
 
 
-def send(file_to_send):
+def send_and_receive_ack(file_to_send, socket_for_send):
 
-    global id_next_elem_in_window, start_time, my_thread
-    start_time = STOP_TIME
-    my_thread = _thread.allocate_lock()
-    id_next_elem_in_window = 0
+    global id_first_elem_in_window, start_time, my_thread
     id_next_elem_to_send = 0
     packet_index = 0
 
-    # Bind the socket to address. The socket must not already be bound.
-    # (The format of address depends on the address family — see above.)
-    # Raises an auditing event socket.bind with arguments self, address.
-
-    socket_for_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    socket_for_send.bind(packetS.SOURCE_ADDRESS)
     buffer, number_packets = create_buffer(file_to_send)
 
     # print("-------------------\n")
     # for pack in buffer:
     #   print(pack.data)
-    # print("-------------------\n")                
-               
-    
+    # print("-------------------\n")
     print("to send")
 
     _thread.start_new_thread(receive, (socket_for_send, ))
 
-    while id_next_elem_in_window < number_packets :   
-         # lock.acquire(waitflag=1, timeout=- 1) -> lock unconditionally, if necessary waiting until it is released by another thread
+    while id_first_elem_in_window < number_packets :   
+        # lock.acquire(waitflag=1, timeout=- 1) -> lock unconditionally, if necessary waiting until it is released by another thread
         # lock.release() -> free the lock. The lock must have been acquired earlier, but not necessarily by the same thread.
-
-        my_thread.acquire()     
-        while id_next_elem_in_window + WINDOW_SIZE < id_next_elem_to_send:
+        my_thread.acquire()
+        while id_first_elem_in_window + WINDOW_SIZE < id_next_elem_to_send:
             if buffer[id_next_elem_to_send].flag == packetS.State.ON_HOLD:
                 start_time = time.time()
                 buffer[id_next_elem_to_send].flag = packetS.State.SEND
@@ -98,18 +90,17 @@ def send(file_to_send):
         if time.time() - start_time >= STOP_TIME:
             print("timeout\n")
             start_time = -1
-            id_next_elem_to_send = id_next_elem_in_window
+            id_next_elem_to_send = id_first_elem_in_window
         else:
             # the time is not out.We receive an ackowledge, the wind will be slide
             print("sliding window")
         my_thread.release()
 
-    socket_for_send.close()
-
 
 def receive(socket_for_send):
+    global id_first_elem_in_window, start_time, my_thread
     while True:
-
+        print("+1+")
         # check what we receive
         packet, client_address = socket_for_send.recvfrom(1024)
         print(packet, client_address)
@@ -119,12 +110,22 @@ def receive(socket_for_send):
         print("it s sent the packet ", acknowledge)
 
         # with each received packet, the window will be slide
-        if acknowledge > id_next_elem_in_window:
+        if acknowledge >= id_first_elem_in_window:
             my_thread.acquire()
-            id_next_elem_in_window = acknowledge
+            id_first_elem_in_window = acknowledge + 1
             start_time = -1
             my_thread.release()
            
+
+def send(file_to_send):
+    # Bind the socket to address. The socket must not already be bound.
+    # (The format of address depends on the address family — see above.)
+    # Raises an auditing event socket.bind with arguments self, address.
+    socket_for_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_for_send.bind(packetS.SOURCE_ADDRESS)
+    send_and_receive_ack(file_to_send, socket_for_send)
+    socket_for_send.close()
+
 
 if __name__ == '__main__':
     send("file_to_send.txt")
