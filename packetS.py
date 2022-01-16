cc3e033e5f9fc7c0c7347c2414d885fc2238647a
petroneladumbrava <petronela.dumbrava@student.tuiasi.ro>
2022-01-16 12:17:46 +0200

    Update packetS

--- packetS.py
+++ packetS.py
@@ -10,8 +10,6 @@ class State(Enum):
 ''' 
     packet structure
     ID_PACKET - packet number 
-    SOURCE_ADDRESS
-    DESTINATION_ADDRESS
     FLAG - ON_HOLD, SEND, RECEIVED - packet state
     DATA - information to send
 '''
@@ -20,17 +18,10 @@ SOURCE_ADDRESS = ('localhost', 8080)
 DESTINATION_ADDRESS = ('localhost', 8081)
 
 class Packet:
-
-    source_address = SOURCE_ADDRESS
-    destination_address = DESTINATION_ADDRESS
-
-
     def __init__(self):
-        id_packet = 1
-        source_address = ('localhost', 8080)
-        destination_address =  ('localhost', 8081)
-        flag = 0
-        data = b''
+        # self.id_packet = 1
+        self.flag = 0
+        self.data = b''
 
 
     def create_packet(self, id_packet, flag):
@@ -38,22 +29,9 @@ class Packet:
         self.flag = flag
 
 
-    def send_packet(self, socket):
-        self.flag = State.SEND
-        id_packet_in_bytes = self.id_packet.to_bytes(4, byteorder='little', signed=True)
-        socket.sendto(self.data + id_packet_in_bytes, self.destination_address)
-
-
-    def receive_packet(self, socket):
-        #functia apelata in receive din SWreceiver
-        #am luat datale primite si am refacut pachetul
-        data_and_id_in_bytes, self.source_address =  socket.recvfrom(4096)
-        self.id_packet = int.from_bytes(data_and_id_in_bytes[0:4], byteorder = 'little', signed = True)
-        self.data = data_and_id_in_bytes[4:]
-
-
     def send_response(self):
         self.flag = State.RECEIVED
+        return
 
 
 
