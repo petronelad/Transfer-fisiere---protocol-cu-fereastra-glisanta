68bd58f2be0f4388fb93ab5a8e921666885f4986
petroneladumbrava <petronela.dumbrava@student.tuiasi.ro>
2022-01-16 18:38:18 +0200

    receiver

--- SWreceiver.py
+++ SWreceiver.py
@@ -1,4 +1,5 @@
 import socket
+from struct import pack
 import packetS
 
 '''
@@ -7,6 +8,14 @@ import packetS
 '''
 
 #primesc pachetul si trimit raspuns
+
+#  def receive_packet( socket):
+#         #functia apelata in receive din SWreceiver
+#         #am luat datale primite si am refacut pachetul
+#         data_and_id_in_bytes, source_address =  socket.recvfrom(4096)
+#         self.id_packet = int.from_bytes(data_and_id_in_bytes[0:4], byteorder = 'little', signed = True)
+#         self.data = data_and_id_in_bytes[4:]
+
 
 def receive(file_received):
     try:
@@ -20,12 +29,18 @@ def receive(file_received):
     socket_received = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     socket_received.bind(packetS.DESTINATION_ADDRESS)
     while True:
-        packet.receive_packet(socket_received)
+
+        # packet.receive_packet(socket_received)
+        data_and_id_in_bytes, source_address =  socket.recvfrom(4096)
+
+        packet.id_packet = int.from_bytes(data_and_id_in_bytes[0:4], byteorder = 'little', signed = True)
+        packet.data = data_and_id_in_bytes[4:]
         if not packet.data:
             break
         id_packet_in_buffer += 1
         packet.flag = packetS.State.RECEIVED
         all_packets.append(packet)
+        print(packet.data)
         file.write(packet.data)
     file.close()
     socket_received.close()
